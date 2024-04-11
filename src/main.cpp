32afec88a73c3733c819f74117e899d20cc75726
Kolesnikov Svyatoslav <k.svyatoslav.2003@inbox.ru>
2024-04-11 18:22:40 +0300

    Отредактирован код

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
@@ -29,18 +28,26 @@ void setup() {
 
 // usart_enable(USART2); //Включение ПУ
 
-// rcc_periph_clock_enable(RCC_GPIOE);
-// gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11|GPIO15); //Режим альтернативной функции
 
 rcc_periph_clock_enable(RCC_ADC12);
 adc_power_off(ADC1);
 rcc_periph_reset_pulse(RST_ADC12);
 
+rcc_periph_clock_enable(RCC_GPIOA);
+gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0); //Режим альтернативной функции
+
 adc_set_clk_prescale(ADC1, ADC_CCR_CKMODE_DIV2);
 // adc_disable_scan_mode(ADC1);
 adc_set_single_conversion_mode(ADC1); //однократное преобразование
-adc_set_sample_time(ADC1, 0, ADC_SMPR_SMP_7DOT5CYC); //канал 0, длительность выборки - 7.5 тактов
+adc_set_sample_time(ADC1, 1, ADC_SMPR_SMP_7DOT5CYC); //канал 0, длительность выборки - 7.5 тактов
+uint8_t channel_array[] = {1}; /* ADC1_IN1 (PA0) */
+adc_set_regular_sequence(ADC1, 1, channel_array);
 adc_power_on(ADC1); //подаём опорное напряжение на преобразователь 
+adc_start_conversion_regular(ADC1); //запуск преобразования (программный)
+while (!adc_eoc(ADC1)); 
+
+rcc_periph_clock_enable(RCC_GPIOE);
+gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9|GPIO11);
 
 }
 void loop() {
@@ -51,18 +58,33 @@ void loop() {
     // usart_send_blocking(USART2, c);
     // for (volatile uint32_t i=0; i<2000000;i++);
     // gpio_toggle(GPIOE, GPIO9);
+    uint32_t res;
+    char str[20];
+    
+    gpio_toggle(GPIOE, GPIO9);
+    
     adc_start_conversion_regular(ADC1); //запуск преобразования (программный)
     while (!adc_eoc(ADC1)); 
+    res = adc_read_regular(ADC1);
+    if (res > 2048) gpio_set(GPIOE, GPIO11);
+    else gpio_clear(GPIOE, GPIO11);
+
+    usart_send_blocking(USART2, res>>8);
+    usart_send_blocking(USART2, res);
+    usart_send_blocking(USART2, "\r");
+    usart_send_blocking(USART2, "\n");
 }
 
-// int main () {
+
+int main () {
     
-//     setup(); //После настройки можно подать сигнал - включить светодиод PE9(порт должен быть настроен)
-//     gpio_set(GPIOE, GPIO15);
-//     while (true) {
-//         loop();
-//     }
-// }
+    
+    setup(); //После настройки можно подать сигнал - включить светодиод PE9(порт должен быть настроен)
+    gpio_set(GPIOE, GPIO15);
+    while (true) {
+        loop();
+    }
+}
 // void usart2_exti26_isr (void){
 //     USART_RQR(USART2) &= ~(USART_RQR_RXFRQ);
 //     // c = static_cast<uint8_t>(usart_recv(USART2));
